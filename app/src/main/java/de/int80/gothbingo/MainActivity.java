4c2e183e02c13279b50740f8516effa524523b67
Jens Kehne <jens@int80.de>
2017-09-23 13:54:12 +0200

    Initial layout

---
+++ app/src/main/java/de/int80/gothbingo/MainActivity.java
@@ -0,0 +1,13 @@
+package de.int80.gothbingo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
