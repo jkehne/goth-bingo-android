46a2c82abbf400d3f9580930ab1a99259c3c581f
Jens Kehne <jens@int80.de>
2017-09-29 13:12:08 +0200

    Add a special class for Bingo fields to make fields clickable

---
+++ app/src/main/java/de/int80/gothbingo/BingoFieldView.java
@@ -0,0 +1,47 @@
+package de.int80.gothbingo;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+/**
+ * Created by jens on 29.09.17.
+ */
+
+public class BingoFieldView extends AppCompatTextView {
+    private int x;
+    private int y;
+
+    public int getFieldX() {
+        return x;
+    }
+
+    public int getFieldY() {
+        return y;
+    }
+
+    private boolean checked = false;
+
+    public BingoFieldView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BingoFieldView);
+        x = a.getInteger(R.styleable.BingoFieldView_coordX, 0);
+        y = a.getInteger(R.styleable.BingoFieldView_coordY, 0);
+    }
+
+    public void toggle() {
+        if (checked) {
+            this.setTextColor(Color.BLACK);
+            this.setBackgroundColor(Color.WHITE);
+            checked = false;
+        } else {
+            this.setTextColor(Color.WHITE);
+            this.setBackgroundColor(Color.parseColor("#666666"));
+            checked = true;
+        }
+    }
+}
--- app/src/main/java/de/int80/gothbingo/MainActivity.java
+++ app/src/main/java/de/int80/gothbingo/MainActivity.java
@@ -2,6 +2,7 @@ package de.int80.gothbingo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBingoFieldClick (View view) {
+        if (view instanceof BingoFieldView)
+            ((BingoFieldView)view).toggle();
     }
 }
