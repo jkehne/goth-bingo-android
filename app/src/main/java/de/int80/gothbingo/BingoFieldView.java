1a71016a381d5993c63012f5971e3f816089f694
Jens Kehne <jens@int80.de>
2017-10-03 16:51:04 +0200

    Extend TextView instead of AppCompatTextView
    Android Studio says this is wrong, but extending AppCompatTextView causes an exception on each click on Android <5.0

--- app/src/main/java/de/int80/gothbingo/BingoFieldView.java
+++ app/src/main/java/de/int80/gothbingo/BingoFieldView.java
@@ -4,14 +4,14 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
+import android.widget.TextView;
 
 /**
  * Created by jens on 29.09.17.
  */
 
-public class BingoFieldView extends AppCompatTextView {
+public class BingoFieldView extends TextView {
     private int x;
     private int y;
 
