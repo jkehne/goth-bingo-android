a4d52f6b15a519decee385634b19ba6002bbf2bb
Jens Kehne <jens@int80.de>
2017-10-14 10:21:01 +0200

    Add signin activity

---
+++ app/src/main/java/de/int80/gothbingo/LoginActivity.java
@@ -0,0 +1,54 @@
+package de.int80.gothbingo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    static final String PLAYER_NAME_KEY = LoginActivity.class.getName() + ".PLAYER_NAME";
+    static final String GAME_ID_KEY = LoginActivity.class.getName() + ".GAME_ID";
+    private static final String LOG = LoginActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button signInButton = (Button)findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleSignIn();
+            }
+        });
+
+        EditText gameIDField = (EditText)findViewById(R.id.gameID);
+        gameIDField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionID, KeyEvent keyEvent) {
+                handleSignIn();
+                return true;
+            }
+        });
+    }
+
+    private void handleSignIn() {
+        EditText playerNameField = (EditText) findViewById(R.id.playerName);
+        EditText gameIDField = (EditText) findViewById(R.id.gameID);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(PLAYER_NAME_KEY, playerNameField.getText().toString());
+        intent.putExtra(GAME_ID_KEY, gameIDField.getText().toString());
+        startActivity(intent);
+    }
+
+}
