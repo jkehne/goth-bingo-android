0b1c65c8d275b719cecf4e7ddae75df0345bfa25
Jens Kehne <jens@int80.de>
2017-10-14 11:13:05 +0200

    Use OkHTTP to download bingo fields

--- app/src/main/java/de/int80/gothbingo/FieldContentFetcher.java
+++ app/src/main/java/de/int80/gothbingo/FieldContentFetcher.java
@@ -8,15 +8,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
-import javax.net.ssl.HttpsURLConnection;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by jens on 02.10.17.
@@ -26,17 +24,7 @@ public class FieldContentFetcher extends AsyncTask<Void, Void, ArrayList<String>
     private static final String TAG = FieldContentFetcher.class.getSimpleName();
 
     private ProgressDialog dialog;
-    private static final URL fieldsURL;
-
-    static {URL fieldsURL1;
-        try {
-            fieldsURL1 = new URL("https://int80.de/bingo/js/fields.php?pure_json=1");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            fieldsURL1 = null;
-        }
-        fieldsURL = fieldsURL1;
-    }
+    private static final String fieldsURL = "https://int80.de/bingo/js/fields.php?pure_json=1";
 
     private MainActivity parentActivity;
 
@@ -53,32 +41,25 @@ public class FieldContentFetcher extends AsyncTask<Void, Void, ArrayList<String>
         super.onPreExecute();
     }
 
-    private String getContentsString(URL url) throws IOException {
+    private String getContentsString(String url) throws IOException {
         GameState state = parentActivity.getState();
 
-        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        Request.Builder builder = new Request.Builder();
+        builder.url(url);
 
         if (state.getLastEtag() != null)
-            connection.setRequestProperty("If-None-Match", state.getLastEtag());
+            builder.header("If-None-Match", state.getLastEtag());
 
-        connection.connect();
+        Response response = new OkHttpClient().newCall(builder.build()).execute();
 
-        if (connection.getResponseCode() == HttpURLConnection.HTTP_NOT_MODIFIED)
-            return "";
-        else if (connection.getResponseCode() != HttpURLConnection.HTTP_OK)
+        if (!response.isSuccessful())
             throw new IOException();
+        else if (response.networkResponse().code() == HttpURLConnection.HTTP_NOT_MODIFIED)
+            return "";
 
-        state.setLastEtag(connection.getHeaderField("ETag"));
+        state.setLastEtag(response.header("Etag"));
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-        StringBuilder result = new StringBuilder();
-        String line;
-
-        while ((line = reader.readLine()) != null)
-            result.append(line);
-
-        return result.toString();
+        return response.body().string();
     }
 
     private ArrayList<String> buildFieldsList(String fieldsString) {
